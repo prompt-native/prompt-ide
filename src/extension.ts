f21564447827a63dfde0ea27116526f563e81f7d
Riguz Lee <dr@riguz.com>
2023-06-06 20:01:08 +0800

    added preview page demo

--- src/extension.ts
+++ src/extension.ts
@@ -5,13 +5,37 @@ import * as vscode from 'vscode';
 import { PromptExplorer } from './prompt/promptExplorer';
 
 export function activate(context: vscode.ExtensionContext) {
-	const rootPath = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0))
-		? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
+    const rootPath = (vscode.workspace.workspaceFolders && (vscode.workspace.workspaceFolders.length > 0))
+        ? vscode.workspace.workspaceFolders[0].uri.fsPath : undefined;
 
-	// Samples of `window.registerTreeDataProvider`
-	const nodeDependenciesProvider = new PromptExplorer(rootPath);
-	vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
-	vscode.commands.registerCommand('nodeDependencies.refreshEntry', () => nodeDependenciesProvider.refresh());
-	vscode.commands.registerCommand('extension.openPackageOnNpm', moduleName => vscode.commands.executeCommand('vscode.open', vscode.Uri.parse(`https://www.npmjs.com/package/${moduleName}`)));
-	vscode.commands.registerCommand('nodeDependencies.addEntry', () => vscode.window.showInformationMessage(`Successfully called add entry.`));
+    // Samples of `window.registerTreeDataProvider`
+    const promptProvider = new PromptExplorer(rootPath);
+    context.subscriptions.push(vscode.window.registerTreeDataProvider('nodeDependencies', promptProvider));
+    context.subscriptions.push(vscode.commands.registerCommand('prompt-studio.helloWorld', () => vscode.window.showInformationMessage(`Success!`)));
+    context.subscriptions.push(vscode.commands.registerCommand('prompt-studio.preview', () => {
+        let panel = vscode.window.createWebviewPanel(
+            'myWebview', // 标识符，需要唯一
+            'My Webview', // 标题
+            vscode.ViewColumn.Two, // 第一列
+            {}
+        );
+        // 设置HTML内容
+        panel.webview.html = getWebviewContent();
+    }));
+
+}
+
+function getWebviewContent() {
+    return `
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hello Webview</title>
+    </head>
+    <body>
+        <h1>Hello Webview!</h1>
+    </body>
+    </html>
+    `;
 }
