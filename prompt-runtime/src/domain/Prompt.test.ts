aebe2a0ef894257ee6b937f5b4fb339f1bf4e0c1
Riguz Lee <dr@riguz.com>
2023-06-26 14:17:27 +0800

    added billder

--- prompt-runtime/src/domain/Prompt.test.ts
+++ prompt-runtime/src/domain/Prompt.test.ts
@@ -1,5 +1,31 @@
-describe('sum module', () => {
-    test('adds 1 + 2 to equal 3', () => {
-        expect(3).toBe(3);
+import { StructuredExampleBuilder } from "./Prompt";
+
+describe('build structued examples', () => {
+    test('init example', () => {
+        let builder = new StructuredExampleBuilder();
+        const result = builder.toStructuredExamples();
+        expect(result.labels).toBe(undefined);
+        expect(result.test).toStrictEqual({ "input": "" });
+        expect(result.examples).toStrictEqual([]);
+    });
+
+    test('add field', () => {
+        let builder = new StructuredExampleBuilder();
+        builder.addField();
+        const result = builder.toStructuredExamples();
+        expect(result.labels).toBe(undefined);
+        expect(result.test).toStrictEqual({ "input1": "", "input2": "" });
+        expect(result.examples).toStrictEqual([]);
+    });
+
+    test('add field with value', () => {
+        let builder = new StructuredExampleBuilder();
+        builder.setTest("input1", "abc");
+        builder.addField();
+        builder.addField();
+        const result = builder.toStructuredExamples();
+        expect(result.labels).toBe(undefined);
+        expect(result.test).toStrictEqual({ "input1": "abc", "input2": "", "input3": "" });
+        expect(result.examples).toStrictEqual([]);
     });
 });
--- prompt-runtime/src/domain/Prompt.ts
+++ prompt-runtime/src/domain/Prompt.ts
@@ -38,6 +38,109 @@ export class Prompt {
     }
 }
 
+export class StructuredExampleBuilder {
+    private _labels: Record<string, string>;
+    private _test: Record<string, string>;
+    private _examples: Record<string, string>[];
+
+    constructor() {
+        this._labels = {};
+        this._test = { "input1": "" };
+        this._examples = [];
+    }
+
+    addField() {
+        const key = `input${Object.keys(this._test).length + 1}`;
+        this._test[key] = "";
+        this._examples.forEach(example => {
+            example[key] = "";
+        });
+    }
+
+    deleteField(name: string) {
+        delete this._test[name];
+        this._examples.forEach(example => {
+            delete example[name];
+        });
+    }
+
+    addExample() {
+        let example = {};
+        for (let key in this._test) {
+            example[key] = "";
+        }
+        example["output"] = "";
+        this._examples.push(example);
+    }
+
+    setExample(index: number, key: string, value: string) {
+        if (index >= this._examples.length) {
+            throw new Error("Index out of range");
+        }
+        this._examples[index][key] = value;
+    }
+
+    setTest(name: string, value: string) {
+        if (!(name in this._test)) {
+            throw new Error("Field not found");
+        }
+        this._test[name] = value;
+    }
+
+    setLabel(name: string, label: string) {
+        if (!(name in this._test)) {
+            throw new Error("Field not found");
+        }
+        this._labels[name] = label;
+    }
+
+    removeLabel(name: string) {
+        if (!(name in this._test)) {
+            throw new Error("Field not found");
+        }
+        delete this._labels[name];
+    }
+
+    toStructuredExamples() {
+        let test: Record<string, string> = {};
+        let examples: Record<string, string>[] = [];
+        let labels: Record<string, string> = {};
+
+        let keyMapping: Record<string, string> = {};
+        let count = 1;
+        let keys = Object.keys(this._test);
+        keys.sort();
+
+        keys.forEach(key => {
+            const newKey = Object.keys(this._test).length === 1 ? "input" : `input${count}`;
+            keyMapping[key] = newKey;
+            test[newKey] = this._test[key];
+            count += 1;
+        });
+
+        for (let key in this._labels) {
+            const newKey = keyMapping[key];
+            labels[newKey] = this._labels[key];
+        }
+
+        examples = this._examples.map(item => {
+            let example: Record<string, string> = {};
+            for (let key in item) {
+                const newKey = keyMapping[key];
+                example[newKey] = item[key];
+            }
+            return example;
+        });
+
+        if (Object.keys(labels).length === 0) {
+            labels = undefined;
+        }
+
+        return new StructuredExamples(examples, test, labels);
+    }
+
+}
+
 export class StructuredExamples {
     constructor(
         public examples: Record<string, string>[],
