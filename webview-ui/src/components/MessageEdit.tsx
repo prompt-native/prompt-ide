d1c76ad32b221954cad8d968dff5e6f875a8c4a7
Riguz Lee <dr@riguz.com>
2024-01-09 21:22:21 +0800

    updated ui

--- webview-ui/src/components/MessageEdit.tsx
+++ webview-ui/src/components/MessageEdit.tsx
@@ -1,9 +1,8 @@
-import { VSCodeTextArea } from "@vscode/webview-ui-toolkit/react";
 import hljs from "highlight.js";
 import json from "highlight.js/lib/languages/json";
 import "highlight.js/styles/default.css";
 import { Message } from "prompt-schema";
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Highlight from "react-highlight";
 
 hljs.registerLanguage("json", json);
@@ -17,6 +16,10 @@ interface MessageProps {
     rows?: number;
 }
 
+function capitalizeFirstChar(str: string): string {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 function MessageEdit({ index, message }: MessageProps) {
     const [editing, setEditing] = useState(false);
     const textAreaRef = useRef<HTMLInputElement>(null);
@@ -28,44 +31,13 @@ function MessageEdit({ index, message }: MessageProps) {
         }
     }, [editing, textAreaRef.current]);
 
-    function adjustHeight() {
-        if (textAreaRef.current) {
-            const shadowRoot = textAreaRef.current.shadowRoot;
-            if (shadowRoot) {
-                const shadowTextArea = shadowRoot.querySelector("textarea");
-                if (shadowTextArea) {
-                    const h = shadowTextArea.scrollHeight;
-                    setHeight(h);
-                    console.log("set", `${h}`);
-                }
-            }
-        }
-    }
-
-    useLayoutEffect(adjustHeight, []);
-
     /**
         vscode-light - Light themes.
         vscode-dark - Dark themes.
         vscode-high-contrast - High contrast themes.
      */
     console.log(document.body.className);
-    if (editing)
-        return (
-            <div className="flex">
-                <div>User</div>
-                <VSCodeTextArea
-                    rows={3}
-                    className="fill"
-                    // @ts-ignore
-                    ref={textAreaRef}
-                    onChange={() => adjustHeight()}
-                    autoFocus
-                    //onBlur={() => setEditing(false)}
-                    initialValue={message.content || ""}
-                />
-            </div>
-        );
+
     const contentBuilder = (content: string, isJson: boolean) => {
         if (!isJson) return <div>{content}</div>;
         return <Highlight className="json no-margin">{content}</Highlight>;
@@ -76,10 +48,20 @@ function MessageEdit({ index, message }: MessageProps) {
                 <div className={`${message.role}-avatar avatar fixed-48 rounded`} />
             </div>
             <div className="fill flex flex-column align-start justify-start">
-                <div className="bold vs-forground mb-10">{message.role} </div>
+                <div className="bold vs-forground mb-10">
+                    {capitalizeFirstChar(message.name || message.role)}
+                </div>
                 {message.content && contentBuilder(message.content, message.role == "function")}
                 {message.function_calls &&
-                    contentBuilder(message.function_calls![0].arguments, true)}
+                    message.function_calls.map((f) => (
+                        <>
+                            <div className="mb-10 flex align-center italic">
+                                <span className={`codicon codicon-live-share mr-10 default`}></span>
+                                {f.name}
+                            </div>
+                            {contentBuilder(f.arguments, true)}
+                        </>
+                    ))}
             </div>
         </div>
     );
